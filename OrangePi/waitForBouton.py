40bed06ba04afca439ed8c95d3cd6db601e41653
Raphaël M-P <rmarquispelletier@gmail.com>
2019-04-04 21:06:08 -0400

    Attendre pour le bouton

---
+++ OrangePi/waitForBouton.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import time, os
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+r = 0
+
+
+def initiatePin(): #Initialiser les GPIO
+	gpio.init()
+	pin = port.PG7
+
+	gpio.setcfg(pin, gpio.OUTPUT)
+	gpio.input(pin)
+
+	gpio.setcfg(pin, 0)
+	gpio.pullup(pin, 0)
+	gpio.pullup(pin, gpio.PULLDOWN)
+	gpio.pullup(pin, gpio.PULLUP)
+
+def buttonState(): #Retourne l'état de la pin
+	return gpio.input(port.PG7)
+
+initiatePin() # beginning of script, enable the GPIO pin.
+
+
+try:
+    while (r == 0):
+        if buttonState() == 1:
+            print "Bouton appuyé"
+            r = 1
+
+        time.sleep(0.1)
+except Exception as e:
+    raise
