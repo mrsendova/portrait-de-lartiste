a1dbab2a5214924c704ba2af524caea5f738d5a0
Raphaël M-P <rmarquispelletier@gmail.com>
2019-05-05 21:24:25 -0400

    +3 inputs

--- readInputs/readInputs.ino
+++ readInputs/readInputs.ino
@@ -1,11 +1,14 @@
 // Déclaration des constantes
-const int switchA = 4;
-const int switchB = 5;
-const int switchC = 6;
-const int switchDA = 7;
-const int switchDB = 8;
-const int switchEA = 9;
-const int switchEB = 10;
+const int switchA = 11;
+const int switchB = 12;
+const int switchC = 3;
+const int switchDA = 4;
+const int switchDB = 5;
+const int switchEA = 6;
+const int switchEB = 7;
+const int switchFA = 8;
+const int switchFB = 9;
+const int switchG = 10;
 int rheoA = 0;
 int rheoB = 1;
 int temp = 2;
@@ -17,6 +20,9 @@ String sDA = "0";
 String sDB = "0";
 String sEA = "0";
 String sEB = "0";
+String sFA = "0";
+String sFB = "0";
+String sG = "0";
 String rA = "0";
 String rB = "0";
 String temperature = "0";
@@ -32,6 +38,9 @@ void setup() {
   pinMode(switchDB, INPUT);
   pinMode(switchEA, INPUT);
   pinMode(switchEB, INPUT);
+  pinMode(switchFA, INPUT);
+  pinMode(switchFB, INPUT);
+  pinMode(switchG, INPUT);
 }
 
 void loop() {
@@ -43,11 +52,14 @@ void loop() {
   sDB = String(digitalRead(switchDB));
   sEA = String(digitalRead(switchEA));
   sEB = String(digitalRead(switchEB));
+  sFA = String(digitalRead(switchFA));
+  sFB = String(digitalRead(switchFB));
+  sG = String(digitalRead(switchG));
   rA = String(analogRead(rheoA));
   rB = String(analogRead(rheoB));
   temperature = String(getTemperature(temp));
 
-  String toReturn = sA + ";" + sB + ";" + sC + ";" + sDA + ";" + sDB + ";" + sEA + ";" + sEB + ";" + rA + ";" + rB + ";" + temperature;
+  String toReturn = sA + ";" + sB + ";" + sC + ";" + sDA + ";" + sDB + ";" + sEA + ";" + sEB + ";" + sFA + ";" + sFB + ";" + sG + ";" + rA + ";" + rB + ";" + temperature;
   Serial.println(toReturn);
   
   delay(100);
